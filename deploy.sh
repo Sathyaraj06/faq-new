4b4c491196ae650883929f845207a5f9c0afa83c
sathyaraj <sathyaraj06>
2023-04-02 08:00:52 +0000

    .sh

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+echo "started---------------"
+
+sudo docker rm -f $(sudo docker ps -aq)
+
+sudo docker rmi $(sudo docker images -q)
+
+git pull
+
+sudo docker-compose build
+
+sudo docker-compose up -d
+
+
+echo "ended---------------"
